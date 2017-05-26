d6bdee1536e08bcd03fba8b3d8c62036a61fa0d9
Fedor A. Fetisov <faf@oits.ru>
2017-05-26 16:35:49 +0300

    Bump version

--- Plugin.php
+++ Plugin.php
@@ -137,7 +137,7 @@ class Plugin extends \Mibew\Plugin\AbstractPlugin implements \Mibew\Plugin\Plugi
      */
     public static function getVersion()
     {
-        return '1.0.1';
+        return '1.1.0';
     }
 
     /**
